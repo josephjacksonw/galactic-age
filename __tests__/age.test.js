89dbc7aa2a1bd56151fd15945dcde3053d07492d
josephjacksonw <josephjacksonw@gmail.com>
2022-09-02 15:49:46 -0400

    refactor old tests to pass after updated functions

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -15,16 +15,16 @@ describe('Age', () => {
 
     test("should return the amount of years left for their life expectancy", () => {
       let input = new Age(30, "average")
-      expect(input.lifeExpected()).toEqual(50)
+      expect(input.lifeExpected()).toEqual([50, 208.33, 80.65, 26.6, 4.22])
     })
 
     test("should return the amount of years over the life expectancy if older that in", () => {
       let input = new Age(90, "average")
-      expect(input.lifeExpected()).toEqual(10)
+      expect(input.lifeExpected()).toEqual([10, 41.67, 16.13, 5.32, 0.84])
     })
 
     test("should return the life expectancy for all other planets", () => {
       let input = new Age(30, "average")
-      expect(input.lifeExpected()).toEqual([50, 208.33, 80.64, 26.59, 4.21])
+      expect(input.lifeExpected()).toEqual([50, 208.33, 80.65, 26.6, 4.22])
     })
 });
