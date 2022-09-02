8a6afe6a6377c9c04ce8bfa7558fb3c6bfdbbdf9
josephjacksonw <josephjacksonw@gmail.com>
2022-09-02 19:20:38 -0400

    MVP built

--- src/index.js
+++ src/index.js
@@ -6,16 +6,14 @@ import Age from './age.js';
 function handleForm(e) {
   e.preventDefault();
   let age = document.getElementById("age").value;
-  let lifestyle = document.getElementById('input[name="lifestyle"]:checked');
+  let lifestyle = document.querySelector('input[name="lifestyle"]:checked').value;
   let input = new Age(age, lifestyle);
   let galacticAge = input.getAge();
   let expectedLife = input.lifeExpected();
   let here1 = document.getElementById("here1");
   let here2 = document.getElementById("here2");
-  console.log("galactic age: " + galacticAge)
-  console.log("expected life: " + expectedLife)
   here1.innerText = galacticAge;
-  here2.innerText == expectedLife;
+  here2.innerText = expectedLife;
 }
 
 document.querySelector("form#life").addEventListener("submit", handleForm);
