56b70e7215e5a8160db1fae0673475f60a216d7e
josephjacksonw <josephjacksonw@gmail.com>
2022-09-02 14:48:17 -0400

    getAge() test good pass

--- src/age.js
+++ src/age.js
@@ -5,14 +5,14 @@ export default class Age {
   }
 
   getAge() {
-    // let mercury = this.age/.24
-    // mercury = Number(mercury.toFixed(2))
-    // let venus = this.age/.62
-    // venus = Number(venus.toFixed(2))
-    // let mars = this.age/1.88
-    // mars = Number(mars.toFixed(2))
-    // let jupiter = this.age/11.86
-    // jupiter = Number(jupiter.toFixed(2))
-    // return [mercury, venus, mars, jupiter]
+    let mercury = this.age/.24
+    mercury = Number(mercury.toFixed(2))
+    let venus = this.age/.62
+    venus = Number(venus.toFixed(2))
+    let mars = this.age/1.88
+    mars = Number(mars.toFixed(2))
+    let jupiter = this.age/11.86
+    jupiter = Number(jupiter.toFixed(2))
+    return [mercury, venus, mars, jupiter]
   }
 }
