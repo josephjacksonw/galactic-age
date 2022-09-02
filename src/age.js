e43d3d9aeb2e07a2886155c94e6a20b45ad825c9
josephjacksonw <josephjacksonw@gmail.com>
2022-09-02 18:22:32 -0400

    basic user logic functioning

--- src/age.js
+++ src/age.js
@@ -2,39 +2,38 @@ const expectedLife = {
   healthy: 90,
   average: 80,
   unhealthy: 70
-}
+};
 
 export default class Age {
   constructor(age, lifestyle) {
-     this.age = age;
-     this.lifestyle = lifestyle;
+    this.age = age;
+    this.lifestyle = lifestyle;
   }
 
   getAge() {
-    let mercury = this.age/.24
-    mercury = Number(mercury.toFixed(2))
-    let venus = this.age/.62
-    venus = Number(venus.toFixed(2))
-    let mars = this.age/1.88
-    mars = Number(mars.toFixed(2))
-    let jupiter = this.age/11.86
-    jupiter = Number(jupiter.toFixed(2))
-    return [mercury, venus, mars, jupiter]
+    let mercury = this.age/.24;
+    mercury = Number(mercury.toFixed(2));
+    let venus = this.age/.62;
+    venus = Number(venus.toFixed(2));
+    let mars = this.age/1.88;
+    mars = Number(mars.toFixed(2));
+    let jupiter = this.age/11.86;
+    jupiter = Number(jupiter.toFixed(2));
+    return [mercury, venus, mars, jupiter];
   }
 
   lifeExpected() {
-    let expected = expectedLife[this.lifestyle]
-    let outcome = Math.abs(expected - this.age)
-    let mercury = outcome/.24
-    mercury = Number(mercury.toFixed(2))
-    let venus = outcome/.62
-    venus = Number(venus.toFixed(2))
-    let mars = outcome/1.88
-    mars = Number(mars.toFixed(2))
-    let jupiter = outcome/11.86
-    jupiter = Number(jupiter.toFixed(2))
-    let outArray = [outcome, mercury, venus, mars, jupiter]
-    return outArray
+    let expected = expectedLife[this.lifestyle];
+    let outcome = Math.abs(expected - this.age);
+    let mercury = outcome/.24;
+    mercury = Number(mercury.toFixed(2));
+    let venus = outcome/.62;
+    venus = Number(venus.toFixed(2));
+    let mars = outcome/1.88;
+    mars = Number(mars.toFixed(2));
+    let jupiter = outcome/11.86;
+    jupiter = Number(jupiter.toFixed(2));
+    let outArray = [outcome, mercury, venus, mars, jupiter];
+    return outArray;
   }
-
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Age from './age.js';
 
+function handleForm(e) {
+  e.preventDefault();
+  let age = document.getElementById("age").value;
+  let lifestyle = document.getElementById('input[name="lifestyle"]:checked');
+  let input = new Age(age, lifestyle);
+  let galacticAge = input.getAge();
+  let expectedLife = input.lifeExpected();
+  let here1 = document.getElementById("here1");
+  let here2 = document.getElementById("here2");
+  console.log("galactic age: " + galacticAge)
+  console.log("expected life: " + expectedLife)
+  here1.innerText = galacticAge;
+  here2.innerText == expectedLife;
+}
+
+document.querySelector("form#life").addEventListener("submit", handleForm);
 
