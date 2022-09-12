ce5cdb8fc545d46578416344f1f9d8a2833a1089
josephjacksonw <josephjacksonw@gmail.com>
2022-09-11 23:30:25 -0400

    refactored tests to reflect changes to code. Deleted unnecessary test

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -15,16 +15,11 @@ describe('Age', () => {
 
     test("should return the amount of years left for their life expectancy", () => {
       let input = new Age(30, "average")
-      expect(input.lifeExpected()).toEqual([50, 208.33, 80.65, 26.6, 4.22])
+      expect(input.lifeExpected()).toEqual([50, 208.33, 80.65, 26.6, 4.22, 50])
     })
 
     test("should return the amount of years over the life expectancy if older that in", () => {
       let input = new Age(90, "average")
-      expect(input.lifeExpected()).toEqual([10, 41.67, 16.13, 5.32, 0.84])
-    })
-
-    test("should return the life expectancy for all other planets", () => {
-      let input = new Age(30, "average")
-      expect(input.lifeExpected()).toEqual([50, 208.33, 80.65, 26.6, 4.22])
+      expect(input.lifeExpected()).toEqual([10, 41.67, 16.13, 5.32, 0.84, -10])
     })
 });
